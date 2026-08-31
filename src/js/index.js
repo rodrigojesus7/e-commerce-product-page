add9f235516eb2bf0df50826a1bc6ef2a376f7a5
Rodrigo Costa <rodrigojesusdacosta@gmail.com>
2026-08-31 17:12:29 -0300

    refactor: refactor image switch js

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,28 @@
 const smallImages = document.querySelectorAll('.small-image-button')
 
+const thumbnail = document.querySelectorAll('.thumbnail')
+
 const mainImage = document.querySelector('.product-img')
 
 smallImages.forEach((image, index) => {
+
     image.addEventListener('click', () => {
-        selectedImage = index + 1
-        mainImage.src = `../src/images/image-product-${selectedImage}.jpg`;
+
+        clearSelection()
+
+        const thumbnailSrc = image.querySelector('img').src;
+
+        mainImage.src = thumbnailSrc.replace('-thumbnail.jpg', '.jpg')
+
+        thumbnail[index].classList.add('product-small-images--selected')
 
     })
 });
 
+function clearSelection() {
+    thumbnail.forEach(image => {
+        image.classList.remove('product-small-images--selected')
+    })
+}
 
 
